fe4a3b4d9a276e0c1f810e95e1b04af3ca05e293
Abdulbariy-Abdurakhmonov <abdulboriy21business@gmail.com>
2025-06-27 19:10:02 +0300

    Initial Commit

---
+++ NasheedPro/NasheedProApp.swift
@@ -0,0 +1,17 @@
+//
+//  NasheedProApp.swift
+//  NasheedPro
+//
+//  Created by Abdulboriy on 27/06/25.
+//
+
+import SwiftUI
+
+@main
+struct NasheedProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
